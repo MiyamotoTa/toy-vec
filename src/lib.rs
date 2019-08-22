3913e5652239ccc666a25148a917f68ad11815f0
Takahiro Miyamoto <takahiro.miyamoto@glv.co.jp>
2019-08-22 10:01:26 +0900

    参照型の引数を複数とる関数を定義
    ※これは lifetime mismatch になる

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,13 @@ impl<T: Default> ToyVec<T> {
         }
     }
 
+    pub fn get_or(&self, index:usize, default:&T) -> &T {
+        match self.get(index) {
+            Some(v) => v,
+            None => default,
+        }
+    }
+
     fn grow(&mut self) {
         // self.capacityが0のときは、allocate_in_heap(1)で長さ1のBox<[T]>を作成し、self.elementsにセットする
         // self.capacityが1以上のときは、allocate_in_heap(self.capacity() * 2)で現在の2倍の長さのBox<[T]>を生成し、self.elementsにセットする。
